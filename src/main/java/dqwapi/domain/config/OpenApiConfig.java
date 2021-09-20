8901b5e86d94adf46fe24b8e4d58d8e3c316add3
hirooka <hirooka@localhost>
2021-09-20 17:01:42 +0900

    Added springdoc-openapi

---
+++ src/main/java/dqwapi/domain/config/OpenApiConfig.java
@@ -0,0 +1,20 @@
+package dqwapi.domain.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+  @Value("${dqwapi.version}")
+  private String version;
+
+  @Bean
+  public OpenAPI customOpenApi() {
+    return new OpenAPI().info(new Info().title("OpenAPI definition").version(version));
+  }
+
+}
