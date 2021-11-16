9393796039dc7ccf983442862558a999a98cf0ba
hirooka <hirooka@localhost>
2021-11-16 22:37:29 +0900

    try to get CloudPlatform

--- src/main/java/dqwapi/domain/config/OpenApiConfig.java
+++ src/main/java/dqwapi/domain/config/OpenApiConfig.java
@@ -3,20 +3,39 @@ package dqwapi.domain.config;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.cloud.CloudPlatform;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.util.ObjectUtils;
 
+@Slf4j
+@RequiredArgsConstructor
 @Configuration
 public class OpenApiConfig {
 
   @Value("${dqwapi.version}")
   private String version;
 
+  @Value("${dqwapi.fqdn}")
+  private String fqdn;
+
+  private final Environment environment;
+
   @Bean
   public OpenAPI customOpenApi() {
-    // TODO: environmental variable
-    return new OpenAPI().addServersItem(new Server().url("https://dqwapi.hirooka.pro")).info(new Info().title("OpenAPI definition").version(version));
+
+    if (ObjectUtils.isEmpty(CloudPlatform.getActive(environment)) || CloudPlatform.getActive(environment).equals(CloudPlatform.NONE)) {
+      return new OpenAPI().info(new Info().title("OpenAPI definition").version(version));
+    } else {
+      log.info("CloudPlatform: {}", CloudPlatform.getActive(environment).name());
+      return new OpenAPI()
+          .addServersItem(new Server().url("https://" + fqdn))
+          .info(new Info().title("OpenAPI definition").version(version));
+    }
   }
 
 }
