7294d7d4df0c8f215def66879cc1d6aea6460b52
hirooka <dev@hirooka.pro>
2022-11-03 23:30:34 +0900

    Swaggerでgit.build.versionを表示するように設定

--- src/main/java/dqwapi/domain/config/OpenApiConfig.java
+++ src/main/java/dqwapi/domain/config/OpenApiConfig.java
@@ -5,8 +5,10 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.cloud.CloudPlatform;
+import org.springframework.boot.info.GitProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
@@ -16,6 +18,9 @@ import org.springframework.util.ObjectUtils;
 @RequiredArgsConstructor
 @Configuration
 public class OpenApiConfig {
+
+  @Autowired
+  private GitProperties gitProperties;
 
   @Value("${dqwapi.fqdn}")
   private String fqdn;
@@ -28,21 +33,22 @@ public class OpenApiConfig {
   @Bean
   public OpenAPI customOpenApi() {
 
+    final String version = gitProperties.get("build.version");
     final String title = "DQW API";
     final String description = "ドラクエウォークの上級職において最適だと考えられるこころの組み合わせを提案するAPIです。";
     if (ObjectUtils.isEmpty(CloudPlatform.getActive(environment)) || CloudPlatform.getActive(environment).equals(CloudPlatform.NONE)) {
       if (runOnCloud) {
         return new OpenAPI()
             .addServersItem(new Server().url("https://" + fqdn))
-            .info(new Info().title(title).version("").description(description));
+            .info(new Info().title(title).version(version).description(description));
       } else {
-        return new OpenAPI().info(new Info().title(title).version("").description(description));
+        return new OpenAPI().info(new Info().title(title).version(version).description(description));
       }
     } else {
       log.info("CloudPlatform: {}", CloudPlatform.getActive(environment).name());
       return new OpenAPI()
           .addServersItem(new Server().url("https://" + fqdn))
-          .info(new Info().title(title).version("").description(description));
+          .info(new Info().title(title).version(version).description(description));
     }
   }
 
